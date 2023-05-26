8f87ed82c5d77b8e9392883459cd94ba8829a3e5
Josue <josueporto59@gmail.com>
2023-05-25 21:28:41 -0300

    variaveis

---
+++ Variaveis.py
@@ -0,0 +1,16 @@
+'''
+Exemplo de tipos de variaveis exceto boolean
+"a"string,"b"float,"c"int
+'''
+a = ('olá')
+b = 2.4
+c = 4
+
+#str type
+print(a)
+
+#float type
+print(b + b)
+
+#int type
+print(c + c)
