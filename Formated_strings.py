8f76a63c01e707d0362fb66e3947f84c4b2af116
Josue <josueporto59@gmail.com>
2023-05-30 19:35:28 -0300

    formated strings

---
+++ Formated_strings.py
@@ -0,0 +1,12 @@
+nome = "Marcos"
+sobrenome  = "Silva"
+profissao = "progarmador"
+
+#O texto deve ser O Marcos Silva e um exelente programador
+
+texto = 'O ' + nome + ' ' + sobrenome + ' é um exelente ' + profissao 
+print(texto)
+
+#formated string
+texto2 = f"O {nome} {sobrenome} é um exelente {profissao}"
+print(texto2)
