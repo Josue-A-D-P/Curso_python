9a6b26cd7079f034b665d1954793c2bd6a6c4ed1
Josue <josueporto59@gmail.com>
2023-05-30 21:08:20 -0300

    metodos strings

---
+++ Metodos_para_string.py
@@ -0,0 +1,29 @@
+'''Alguns metodos para strings'''
+
+#O metodo .capitalize() retorna uma copia da string com o primeiro caracter em maiusculo
+mensagem = "eu adoro python"
+print(mensagem.capitalize())
+
+#O metodo .casefold() remove todas as diferencas de maiuscula/minuscula de uma string
+mensagem = "EU ADORO PYTHON"
+print(mensagem.casefold())
+
+#O metodo .lower() retorna uma copia da string com todos os caracteres convertidos para minusculo
+mensagem ="EU ADORO PYTHON"
+print(mensagem.lower())
+
+#O metodo .lstrip() retorna uma copia da string com espaços iniciais removidos
+mensagem = "     eu adoro python"
+print(mensagem.lstrip())
+
+#O metodo .split() retorna uma lista de palavras na string
+mensagem = "eu adoro python"
+print(mensagem.split())
+
+#O metodo .title() retorna uma copia da string com todas as palavras começando em maiusculo
+mensagem = "eu adoro python"
+print(mensagem.title())
+
+#O metodo .upper() retorna uma copia da string com todos os caracteres convertidos para maiusculo
+mensagem = "eu adoro python"
+print(mensagem.upper())
