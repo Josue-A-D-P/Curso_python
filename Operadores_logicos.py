f8c99436a994b20f625eae3da6f6ef23b62e0b2d
Josue <josueporto59@gmail.com>
2023-06-01 20:23:19 -0300

    operadores logicos

---
+++ Operadores_logicos.py
@@ -0,0 +1,9 @@
+#Operadores logicos 
+#Simulador de financiamento
+renda_acima_5mil = True
+nome_limpo =True
+
+if renda_acima_5mil and nome_limpo:
+    print('Aprovado')
+else:
+    print('Negado')
