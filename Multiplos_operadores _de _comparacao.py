7feb697859e996b1f0f84c7eedf37f61174477e5
Josue <josueporto59@gmail.com>
2023-06-02 18:30:42 -0300

    multiplos operadores

---
+++ Multiplos_operadores _de _comparacao.py
@@ -0,0 +1,14 @@
+#Multiple comparison operators (Multiplos operadores de comparação)
+
+valor = 19
+
+'''if valor >= 20 and valor <= 40:
+    print('Produto aceito')
+else:
+    print('Produto negado')'''
+
+#codigo mais limpo
+if 20 >= valor <= 40:
+    print('Produto aceito')
+else:
+    print('Produto negado')
