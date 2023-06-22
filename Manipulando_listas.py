47699b31c7c82d9aaca39f36da55a2a8671d531a
Josue <josueporto59@gmail.com>
2023-06-22 19:42:26 -0300

    Listas

---
+++ Manipulando_listas.py
@@ -0,0 +1,15 @@
+#Listas:
+''' Adicionar mais de uma informação em variaveis
+    Manter a sequencia dos dados em uma variavel  '''
+
+
+paises = ['Brasil', 'Portugal', 'Canadá', 'Africa']
+# index      0           1         2          3
+#           -4          -3         -2          -1
+
+
+print(paises[0])
+
+paises[0] = 'Inglaterra'
+
+print(paises[0])
