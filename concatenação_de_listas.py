144c08ef2e19c23f0c67431f4363ac9ed1e3f39d
Josue <josueporto59@gmail.com>
2023-06-22 20:41:55 -0300

    concatenando_listas

---
+++ concatenação_de_listas.py
@@ -0,0 +1,14 @@
+#Listas:
+#Concatenando lista
+
+numeros = [2 ,3 ,6 , 7, 8]
+letras = ['a', 'b', 'c', 'd', 'e']
+
+numeros.extend(letras)
+
+print(numeros)
+
+itens = ['iten1', 'iten2'], ['iten3', 'iten4']
+
+print(itens[1][0])
+print(itens[0][1])
