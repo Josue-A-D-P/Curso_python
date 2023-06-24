5fafae7dccf7955cc9fb8615ee9efabff501ba1f
Josue <josueporto59@gmail.com>
2023-06-23 21:48:57 -0300

    listas_tuple

---
+++ listas_tupes.py
@@ -0,0 +1,9 @@
+'''Entendendo tuples 
+   tuples são imutaveis,mais rapidas,e ocupão menos memória'''
+
+
+cores_list = ['amarelo', 'azul', 'vermelho', 'verde']
+cores_tuple = ('amarelo', 'azul', 'vermelho', 'verde')
+
+print(cores_list )
+print(cores_tuple )
