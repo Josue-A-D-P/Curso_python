864d27e7a16bbef57ed0fe74bbbe872dc62dc2e3
Josue <josueporto59@gmail.com>
2023-06-27 20:39:45 -0300

    Looping em dicionario

---
+++ Looping_em_dicionario.py
@@ -0,0 +1,17 @@
+# Dicionarios 
+    # Utiliza index no formato de keys e value
+    # Aceita integer float string boolean...
+
+
+aluno = {'Nome': 'Ana', 'Idade': 16, 'Nota_final': 'A', 'Aprovação': True}
+print(aluno)
+print()
+
+
+for x in aluno.values():
+    print(x)
+print()
+
+
+for x in aluno.items():
+    print(x)
