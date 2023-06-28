5f6b773c0e6d517f5abd4330c34e217b8dba684c
Josue <josueporto59@gmail.com>
2023-06-28 20:08:52 -0300

    Map com lambda

---
+++ Map_com_lambda.py
@@ -0,0 +1,13 @@
+# Map function
+    # Muito utilizada com listas
+    # Aplicar uma função a um iterable, por item (lista, tuple ,dic...)
+
+lista1 = [1, 2, 3, 4]
+
+#def mult(x):
+    #return x * 2
+
+#lista2 = map(lambda x: x * 2 ,lista1)
+
+
+print(list(map(lambda x: x * 2 ,lista1)))
