a0775d8a9684b35c9389d25006e86239ff3ed580
Josue <josueporto59@gmail.com>
2023-07-11 06:27:14 -0300

    Erros try except

---
+++ Erros_try_except.py
@@ -0,0 +1,10 @@
+# Erros
+    # Exelenete para testes
+    # Nao realiza o 'stop' no programa
+    # Mensagens customizadas quando encomtra um erro
+
+try:
+    letras = ['a', 'b', 'c']
+    print (letras [3])
+except :IndexError
+print('Index nao existe:')
