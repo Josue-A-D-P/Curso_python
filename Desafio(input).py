ba7d6da5ceb057f7330aa2261ed3eb474e86f871
Josue <josueporto59@gmail.com>
2023-07-20 23:03:28 -0300

    Desafio input

---
+++ Desafio(input).py
@@ -0,0 +1,9 @@
+'''
+Neste desafio quero que voce crie um script que prgunte o nome
+e a idade do usuario usando o comando input(),
+Depois disso o script deve imprimir uma menssagem que diga 
+'Olá [Nome] Você tem [idade] anos' '''
+
+nome = input('Por favor digite seu nome: ')
+idade = input('Agora digite sua idade: ')
+print(f'Olá {nome} Você tem {idade} anos.')
