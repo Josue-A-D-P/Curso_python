dc484cd5b3d5a08c00ddaf3c9b1c46faed60a6f7
Josue <josueporto59@gmail.com>
2023-07-21 00:41:52 -0300

    listas

---
+++ Desafio(listas).py
@@ -0,0 +1,19 @@
+
+# Desafio criando listas
+'''
+Neste desafio gostaria que você criasse uma lista chamada 'frutas'
+que comtenha os seguintes itens: "maçã", "banana", "manga" e "uva"
+depois disso quero que você imprima esta lista na tela.'''
+
+frutas = ['maçã', 'banana', 'manga', 'uva']
+print(frutas)
+print("\n========================\n")
+
+# Desafio Manipulando lista
+
+'''
+Para este desafio quero que você use a lista frutas do desafio
+anterior Seu desafio é imprimir o primeiro e o ultimo iten da lista.
+'''
+print(f'O primeiro iten é :{frutas[0]}')
+print(f'\nO ultimo iten é :{frutas[-1]}')
