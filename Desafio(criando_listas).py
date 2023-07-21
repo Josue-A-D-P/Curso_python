d040a60172aaf1683505049c85b861ac0788e003
Josue <josueporto59@gmail.com>
2023-07-21 00:02:02 -0300

    criando listas

---
+++ Desafio(criando_listas).py
@@ -0,0 +1,7 @@
+'''
+Neste desafio gostaria que você criasse uma lista chamada 'frutas'
+que comtenha os seguintes itens: "maçã", "banana", "manga" e "uva"
+depois disso quero que você imprima esta lista na tela.'''
+
+frutas = ['maçã, banana, manga, uva']
+print(frutas)
