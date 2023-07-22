7dc53d1625e7291438f0ffeaee5a0b0aec1054f2
Josue <josueporto59@gmail.com>
2023-07-22 00:02:17 -0300

    Desafio while loop

---
+++ Desafio(while_loop).py
@@ -0,0 +1,11 @@
+# 1° Desafio While loop (contador 1 a 10)
+
+'''
+Para este desafio quero que você use while loop 
+para imprimir os numeros de 1 a 10 na tela
+'''
+
+num = 0
+while num < 10:
+    num += 1
+    print(num)
