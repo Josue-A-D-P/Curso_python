4a79c66124488cc3481316f7a378fbcd5fc901a2
Josue <josueporto59@gmail.com>
2023-07-25 21:39:19 -0300

    Desafio

--- Desafio(calculo,fatorial).py
+++ Desafio(calculo,fatorial).py
@@ -4,21 +4,11 @@
 Crie uma função que calcule o fatorial de um numero e imprima o resultado na tela .
 '''
 
-def fatorial(num):
-    contador = 0
-    pare = num - 1
-    while num > 1 :
-        contador += 1
-        print(contador)
-        num = num - 1
-        num = num * num
-        print (num)    
-        if contador == pare:
-            break
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fatorial(n - 1)
 
-
-
-
-user_num = int(input("digite um numero: "))
-
-fatorial(user_num)
+user_num = int(input('Digite um numero: '))
+print(f'O fatorial de {user_num} é {fatorial(user_num)}')
